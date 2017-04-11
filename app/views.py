34620ce1251949a93c246f58caa73a6a1f1de06f
SARA SIMON <smbsimon@gmail.com>
2017-04-11 16:17:43 -0400

    remove so much css, build out questions, iteration very early zero of a style

--- app/views.py
+++ app/views.py
@@ -31,30 +31,3 @@ def index():
         landing=landing,
         project_social=project_social,
         page_url=page_url)
-
-
-@app.route("/<Name>")
-def entry_page(Name):
-    for entry in entries:
-        if "slug" in entry and Name == entry["slug"]:
-            entries.remove(entry)
-            entries.insert(0, entry)
-
-    page_url = BASE_URL + request.path
-    page_title = entries[0]["Name"]
-
-    social = {
-        "title": page_title,
-        "subtitle": "",
-        "img": "",
-        "description": "",
-        "twitter_text": "",
-        "twitter_hashtag": ""
-    }
-
-    return render_template("content.html",
-        page_title=page_title,
-        social=social,
-        entries=entries,
-        project_social=project_social,
-        page_url=page_url)
